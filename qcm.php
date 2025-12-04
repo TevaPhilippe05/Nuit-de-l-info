41b1ba2cc7f9fa558ff5b0cd3391addfa1c39e9d
BillyBoy16 <lennyloy07@gmail.com>
2025-12-04 20:49:00 +0100

    feat: add qcm.html / qcm.php / update idée qcm

---
+++ qcm.php
@@ -0,0 +1,27 @@
+<?php
+    // crud et mysql 
+    mysqli_fetch_assoc($rs); // permet d'avoir le resultat de la requette ligne par ligne 
+    // faire une connexion a la bdd 
+    $conn = mysqli_connect("localhost", "cass", "**", "data_base");
+    mysqli_set_charset($conn, "utf8");
+
+    function GetQuestions($conn){
+        $sql="SELECT * FROM questions";
+        $res=mysqli_query($conn, $sql);
+        return $res ;
+    }
+
+    function EstVrai($conn, $id_reponse){
+        $sql="SELECT estVrai FROM reponse WHERE id = $id_reponse";
+        $res=mysqli_query($conn, $sql);
+        return $res ;
+    }
+
+    function GetReponsesByQuestionId($conn, $id_question){
+        $sql="SELECT * FROM reponse WHERE id = $id_question";
+        $res=mysqli_query($conn, $sql);
+        return $res ;
+    }
+    // deconnecter 
+    mysqli_close($conn);
+?>
