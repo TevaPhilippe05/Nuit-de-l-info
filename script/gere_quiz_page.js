2dd66bc78451bca43722bfe965e317a5fd686378
Bluz0 <amouats5@gmail.com>
2025-12-04 23:34:36 +0100

    refactor : move qcm.php to lib/qcm.php and create question_json.php

--- script/gere_quiz_page.js
+++ script/gere_quiz_page.js
@@ -6,7 +6,7 @@ var id_question = urlParams.get("id_question");
 
 id_question = id_question || 1; // valeur par défaut si pas de paramètre
 
-function create(tag, container, text=null) {
+function create(tag, container, text = null) {
     const element = document.createElement(tag);
     element.innerText = text;
     container.appendChild(element);
@@ -26,11 +26,11 @@ let choix_4 = document.querySelector("#rep4");
 
 let question = document.querySelector(".question");
 
-function afficherQuestion(id_question){
+function afficherQuestion(id_question) {
     // fetch sur le json avec id_question en param
 }
 
-function afficherReponsesFromQuestion(id_question){
+function afficherReponsesFromQuestion(id_question) {
     // fetch sur le json
     choix_1.innerText = "Réponse 1"; // mettre les vraies valeurs
     choix_2.innerText = "Réponse 2";
@@ -41,28 +41,70 @@ function afficherReponsesFromQuestion(id_question){
 
 let image = document.querySelector("#img_question");
 
-function afficherImageFromQuestion(id_question){
+function afficherImageFromQuestion(id_question) {
     // fetch sur le json avec id_qestion en param
 }
 
-function reponseFinale(){
+let reponseSelectionee = false;
+let reponseChoix = "";
+
+function choix_reponse() {
+    choix_1.addEventListener("click", function () {
+        reponseSelectionee = true;
+        reponseChoix = choix_1.innerHTML;
+        console.log(reponseChoix);
+    })
+
+    choix_2.addEventListener("click", function () {
+        reponseSelectionee = true;
+        reponseChoix = choix_2.innerHTML;
+        console.log(reponseChoix);
+    })
+
+    choix_3.addEventListener("click", function () {
+        reponseSelectionee = true;
+        reponseChoix = choix_3.innerHTML;
+        console.log(reponseChoix);
+    })
+    choix_4.addEventListener("click", function () {
+        reponseSelectionee = true;
+        reponseChoix = choix_4.innerHTML;
+        console.log(reponseChoix);
+    })
+}
+
+choix_reponse();
+
+function reponseFinale() {
     // fetch sur le json pour savoir si estVrai
 
-    if (reponseSelectionee){
+
+
+    if (reponseSelectionee && verifier_veraciter()) {
         document.querySelector(".resultat").innerHTML = "Bonne réponse !";
     }
-    else{
+    else {
         document.querySelector(".resultat").innerHTML = "Mauvaise réponse !";
     }
 
 }
 
+
+// a activé seulement si true dans reponse avec estVrai
 function questionSuivante() {
-    
-    // a activé seulement si true dans reponse avec estVrai
-    bouton_suivant.addEventListener("click", function() {
-        exemple_id_question += 1; // mettre du get
-        window.location.href = "qcm.html?id_question=" + exemple_id_question;
+    bouton_suivant.addEventListener("click", function () {
+
+        if (!reponseSelectionee) {
+            alert("Veuillez sélectionner une réponse avant de continuer.");
+            window.location.href = "qcm.html?id_question=" + exemple_id_question;
+        
+        }
+        else{
+            exemple_id_question += 1; // mettre du get
+            window.location.href = "qcm.html?id_question=" + exemple_id_question;
+        }
+        
     });
 }
 
+questionSuivante();
