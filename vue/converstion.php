2bd4da61491522a4e6dc2c3ffc14c45cdb7d6118
Lohan Hébert <lohanhebert7@gmail.com>
2025-12-04 20:28:02 +0100

    feat: header

--- vue/converstion.php
+++ vue/converstion.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conversaion</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <?php include 'header.php' ?>
+    coucou
     
 </body>
 </html>
