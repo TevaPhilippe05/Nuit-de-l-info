1771380c3d4b0afb8f2b63a9a3daa6375c2ffbad
BillyBoy16 <lennyloy07@gmail.com>
2025-12-05 02:06:05 +0100

    add: snake

---
+++ snake.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Jeu Snake</title>
+    <link rel="stylesheet" href="style/page-snake.css" />
+</head>
+
+<body>
+    <h1>Jeu Snake</h1>
+    <canvas id="gameCanvas" width="400" height="400"></canvas>
+    <p id="score">Score : 0</p>
+    <p id="meilleur_score">Meilleur Score : 0</p>
+    <p id="message_perdu"></p>
+    <button id="playButton">Play</button>
+    <script src="script/snake.js"></script>
+
+</body>
+
+</html>
