00514ae45ca0636c006b3848db580edb0849437c
Bluz0 <amouats5@gmail.com>
2025-12-05 01:29:36 +0100

    fix : db connexion

--- modele/db.php
+++ modele/db.php
@@ -1,5 +1,8 @@
 <?php
-$pdo = new PDO("mysql:host=127.0.0.1;dbname=forum;charset=utf8mb4", "root", "", [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-]);
+
+    $conn = mysqli_connect("localhost", "root", "", "forum");
+    mysqli_set_charset($conn,"utf8");
+// $pdo = new PDO("mysql:host=127.0.0.1;dbname=forum;charset=utf8mb4", "root", "", [
+//     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+// ]);
 ?>
