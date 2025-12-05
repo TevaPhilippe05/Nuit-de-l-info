42d81e3501fb1f420f0ecab97462ab0d047ace71
philipte <philippe.teva.pro@gmail.com>
2025-12-05 05:48:02 +0100

    test: fix inscription si je sais ecrire

--- db/db_connect.php
+++ db/db_connect.php
@@ -3,7 +3,7 @@
 // error_reporting(E_ALL);
 // ini_set('display_errors', '1');
 function connect(){
-    $conn = mysqli_connect("localhost","root","ubuntu54.38.0.127","connexions");
+    $conn = mysqli_connect("localhost","root","ubuntu54.38.0.127","connexion");
     mysqli_set_charset($conn,"utf8");
     return $conn;
 }
