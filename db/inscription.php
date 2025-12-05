190392127a05d5c8d8962b13e45b7a4d713aa3f9
philipte <philippe.teva.pro@gmail.com>
2025-12-05 05:38:48 +0100

    fix: inscription

--- db/inscription.php
+++ db/inscription.php
@@ -1,26 +1,34 @@
-
 <?php
 include "db_connect.php";
 
 if(isset($_POST['valider'])){
     $pseudo = $_POST['nom_uti'];
     $password = $_POST['password'];
+
+    if(empty($pseudo) || empty($password)){
+        die("Nom d’utilisateur et mot de passe requis !");
+    }
+
     $conn = connect();
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    $sql = "INSERT INTO users (nom_uti, password) VALUES ('$pseudo', '$hashed_password')";
-    mysqli_query($conn, $sql) or die("Erreur SQL ! ".mysqli_error($conn));
+
+    $stmt = mysqli_prepare($conn, "INSERT INTO users (nom_uti, password) VALUES (?, ?)");
+    mysqli_stmt_bind_param($stmt, "ss", $pseudo, $hashed_password);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
     mysqli_close($conn);
 }
 ?>
 <html>
-    <head><title>Formulaire de saisie utilisateur </title></head>
-    <body>
-        <h1>Inscrivez-vous !</h1>
-        <h2>Entrez les données demandées :</h2>
-        <form name="inscription" method="post" action="form.php">
-            Entrez votre nom_uti : <input type="text" name="nom_uti"/> <br/>
-            Entrez votre password : <input type="password" name="password"/><br/>
-            <input type="submit" name="valider" value="OK"/>
-        </form>
-    </body>
+<head><title>Formulaire de saisie utilisateur</title></head>
+<body>
+<h1>Inscrivez-vous !</h1>
+<h2>Entrez les données demandées :</h2>
+<form name="inscription" method="post" action="form.php">
+    Entrez votre nom_uti : <input type="text" name="nom_uti" required/> <br/>
+    Entrez votre password : <input type="password" name="password" required/><br/>
+    <input type="submit" name="valider" value="OK"/>
+</form>
+</body>
 </html>
