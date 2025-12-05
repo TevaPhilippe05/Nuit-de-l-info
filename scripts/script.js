534f2aa4a5c97a4daf8e549aa9cded7ac80abcf9
Harupi <elliothrp73@gmail.com>
2025-12-05 01:14:52 +0100

    chore: Changement du comportement et du style du formulaire pour ajouter une alternative

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+/////////////////////////////////// FORMULAIRES ////////////////////////////////////////
+
+const sel_form_app = document.querySelector("#sel-form-app");
+const txt_form_app = document.querySelector("#txt-form-app");
+const new_form_app = document.querySelector("#new-form-app");
+const btn_sel_conf_app = document.querySelector("#btn-sel-conf-app");
+const btn_new_conf_app = document.querySelector("#btn-new-conf-app");
+const btn_add_app = document.querySelector("#btn-add-app");
+
+const sel_form_alt = document.querySelector("#sel-form-alt");
+const txt_form_alt = document.querySelector("#txt-form-alt");
+const new_form_alt = document.querySelector("#new-form-alt");
+const btn_sel_conf_alt = document.querySelector("#btn-sel-conf-alt");
+const btn_new_conf_alt = document.querySelector("#btn-new-conf-alt");
+const btn_add_alt = document.querySelector("#btn-add-alt");
+
+const barre_progression = document.querySelector(".progression-ajout .barre-ajout");
+
+btn_sel_conf_app.addEventListener("click", function() {
+    sel_form_app.classList.add("cache-form");
+    txt_form_app.classList.add("cache-form");
+    sel_form_alt.classList.remove("cache-form");
+    txt_form_alt.classList.remove("cache-form");
+    barre_progression.style.width = "100%"
+});
+
+btn_new_conf_app.addEventListener("click", function() {
+    new_form_app.classList.add("cache-form");
+    sel_form_alt.classList.remove("cache-form");
+    txt_form_alt.classList.remove("cache-form");
+    barre_progression.style.width = "100%"
+});
+
+btn_add_app.addEventListener("click", function() {
+    sel_form_app.classList.add("cache-form");
+    txt_form_app.classList.add("cache-form");
+    new_form_app.classList.remove("cache-form");
+});
+
+btn_add_alt.addEventListener("click", function() {
+    sel_form_alt.classList.add("cache-form");
+    txt_form_alt.classList.add("cache-form");
+    new_form_alt.classList.remove("cache-form");
+});
--- vue/ajout-alternative.php
+++ vue/ajout-alternative.php
@@ -29,8 +29,8 @@
     <div class="progression-ajout">
         <div class="barre-ajout"></div>
     </div>
-    <form action="http://51.68.91.213/info102/tp2.php" method="POST">
-        <fieldset>
+    <form id="sel-form-app" action="" method="POST">
+        <fieldset class="champ-form-ajout">
             <legend>Application à remplacer</legend>
             <div>
                 <label for="app">Choisissez une application : </label>
@@ -39,23 +39,22 @@
                     <!-- https://stackoverflow.com/questions/8022353/how-to-populate-html-dropdown-list-with-values-from-database -->
                 </select>
             </div>
-            <button type="submit">Confirmer</button>
+            <button id="btn-sel-conf-app" type="button">Confirmer</button>
         </fieldset>
     </form>
-    <p>L'application n'est pas référencée sur le site ? <button>Ajoutez-la !</button></p>
-    <form action="http://51.68.91.213/info102/tp2.php" method="POST">
-        <fieldset>
+    <p id="txt-form-app">L'application n'est pas référencée sur le site ? <button id="btn-add-app">Ajoutez-la !</button></p>
+    <form id="new-form-app" class="cache-form" action="" method="POST">
+        <fieldset class="champ-form-ajout">
             <legend>Application à remplacer</legend>
-            <p><label for="nom-app">Nom : </label><input type="text" name="nom-app" id="nom-app" placeholder="Windows"></p>
+            <p><label for="nom-app">Nom : </label><input type="text" name="nom-app" id="nom-app" placeholder="Windows" required></p>
             <p><label for="logo-app">Logo : </label><input type="file" name="logo-app" id="logo-app"></p>
             <p><label for="description-app">Description : </label><textarea name="description-app" id="description-app" placeholder="Un système d'exploitation"></textarea></p>
             <p><label for="site-app">Site : </label><input type="url" name="site-app" id="site-app" placeholder="https://www.microsoft.com/en-us/windows/"></p>
-            <button type="submit">Confirmer</button>
+            <button id="btn-new-conf-app" type="submit">Confirmer</button>
         </fieldset>
     </form>
-    <div style="height: 40px;"></div> <!-- Supprimer quand il y a du css-->
-    <form action="http://51.68.91.213/info102/tp2.php" method="POST">
-        <fieldset>
+    <form id="sel-form-alt" class="cache-form" action="" method="POST">
+        <fieldset class="champ-form-ajout">
             <legend>Alternative open-source</legend>
             <div>
                 <label for="app">Choisissez une alternative : </label>
@@ -64,19 +63,20 @@
                     <!-- https://stackoverflow.com/questions/8022353/how-to-populate-html-dropdown-list-with-values-from-database -->
                 </select>
             </div>
-            <button type="submit">Confirmer</button>
+            <button id="btn-sel-conf-alt" type="button">Confirmer</button>
         </fieldset>
     </form>
-    <p>L'alternative n'est pas référencée sur le site ? <button>Ajoutez-la !</button></p>
-    <form action="http://51.68.91.213/info102/tp2.php" method="POST">
-        <fieldset>
+    <p id="txt-form-alt" class="cache-form">L'alternative n'est pas référencée sur le site ? <button id="btn-add-alt">Ajoutez-la !</button></p>
+    <form id="new-form-alt" class="cache-form" action="" method="POST">
+        <fieldset class="champ-form-ajout">
             <legend>Alternative open-source</legend>
             <p><label for="nom-alt">Nom : </label><input type="text" name="nom-alt" id="nom-alt" placeholder="Debian"></p>
             <p><label for="logo-alt">Logo : </label><input type="file" name="logo-alt" id="logo-alt"></p>
             <p><label for="description-alt">Description : </label><textarea name="description-alt" id="description-app" placeholder="Un système d'exploitation open-source"></textarea></p>
             <p><label for="site-alt">Site : </label><input type="url" name="site-alt" id="site-alt" placeholder="https://www.debian.org/"></p>
-            <button type="submit">Confirmer</button>
+            <button id="btn-new-conf-alt" type="submit">Confirmer</button>
         </fieldset>
     </form>
+    <script type="text/javascript" src="../scripts/script.js"></script>
 </body>
 </html>
